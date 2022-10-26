a2d1bf354ba61eb57f134a05a81ef00e261de450
Kevin Li <kevin.li20021106@gmail.com>
2022-10-25 22:46:01 -0400

    Complete navbar

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 import { NavLinks } from "../../utils/navbarLink";
 import Logo from "../../assets/apple.svg";
-import Search from "../../assets/search.svg";
 import Bag from "../../assets/bag.svg";
 
 export default function Navbar(): JSX.Element {
   const [toggle, setToggle] = useState(false);
 
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 1080) setToggle(false);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${toggle ? "active" : ""}`}>
+      <div className="navbar-desktop">
+        <img src={Logo} alt="" className="navbar-link" />
+        {NavLinks.map((link) => (
+          <Link key={link.label} to={link.path} className="navbar-link">
+            {link.label}
+          </Link>
+        ))}
+        <img src={Bag} alt="" className="navbar-link" />
+      </div>
+
       <div className="navbar-mobile">
         <div
           onClick={() => setToggle((prev) => !prev)}
@@ -21,18 +40,17 @@ export default function Navbar(): JSX.Element {
           <span className="line line-bottom"></span>
         </div>
         <img src={Logo} alt="" className="navbar-link" />
-        <img src={Bag} alt="" className="navbar-link" />
+        <img src={Bag} alt="" className="navbar-link navbar-bag" />
       </div>
 
-      <div className="navbar-desktop">
-        <img src={Logo} alt="" className="navbar-link" />
-        {NavLinks.map((link) => (
-          <Link key={link.label} to={link.path} className="navbar-link">
-            {link.label}
-          </Link>
-        ))}
-        <img src={Search} alt="" className="navbar-link" />
-        <img src={Bag} alt="" className="navbar-link" />
+      <div style={{ display: toggle ? "block" : "none" }}>
+        <div className={`navbar-collapse ${toggle ? "active" : ""}`}>
+          {NavLinks.map((link) => (
+            <Link key={link.label} to={link.path} className="navbar-link">
+              {link.label}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
